b1980f7a37c78d4edf6de332771af3b30e8c03d2
Orfeas Antoniou <orfeasa@gmail.com>
2020-04-23 18:59:53 +0300

    Implement the order history page

--- main/views.py
+++ main/views.py
@@ -1,20 +1,39 @@
+import datetime
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
-                                  TemplateView, UpdateView)
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
 
 from main import forms
+from main.forms import CategoryForm
+from main.models import Order
 
 
 class PlanView(LoginRequiredMixin, TemplateView):
     template_name = "main/plan.html"
 
 
-class OrderView(LoginRequiredMixin, TemplateView):
+class OrderListView(LoginRequiredMixin, ListView):
+    model = Order
+    context_object_name = "orders"
     template_name = "main/orders.html"
-    # order list view
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = datetime.datetime.now()
+        return context
+
+    def get_queryset(self):
+        return super().get_queryset()
 
 
 class IndexView(TemplateView):
